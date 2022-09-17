b9b3b578cac40a71dcc59bb8045dd86a25626e94
Sourav Khare <souravkhare34@gmail.com>
2022-09-17 09:49:37 +0530

    [Sourav]- Program to take three inputs -a,b and c

---
+++ arithmeticUC1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+declare -A dictionary
+
+read -p "Enter num1: " a ;
+read -p "Enter num2: " b ;
+read -p "Enter num3: " c ;
+
+
+echo "num1 = $a" ;
+echo "num2 = $b" ;
+echo "num3 = $c" ;
+
+
