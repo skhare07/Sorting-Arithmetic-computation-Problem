584fd3ae937cf447354b66a923058a59a4a23a90
Sourav Khare <souravkhare34@gmail.com>
2022-09-17 10:07:35 +0530

    [Sourav]- Program to store values from dictionary to array after computation

---
+++ arithmeticUC7.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+declare -A dictionary
+
+read -p "Enter num1: " a ;
+read -p "Enter num2: " b ;
+read -p "Enter num3: " c ;
+
+
+echo "num1 = $a" ;
+echo "num2 = $b" ;
+echo "num3 = $c" ;
+
+
+z=$(( $a + $b * $c ))
+dictionary[1]=$z;
+echo $z;
+
+y=$(( $a * $b + $c ))
+dictionary[2]=$y;
+echo $y;
+
+x=$(( $c + $a / $b ))
+dictionary[3]=$x;
+echo $x;
+
+w=$(( $a % $b + $c ))
+dictionary[4]=$w;
+echo $w;
+echo " ";
+
+
+d=${dictionary[@]};
+echo "dictionary : $d";
+
+
+echo " ";
+
+for (( i=1 ; i<=4 ; i++ ))
+do
+
+array[(($i))]=${dictionary[$i]};
+
+done
+
+arr="${array[@]}" ;
+
+echo "array : $arr";
+
+
